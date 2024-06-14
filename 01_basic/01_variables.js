abb556e357fb58041c759cde0e04e408b803746e
M bhandari <144377289+M02bhandari@users.noreply.github.com>
2024-06-14 09:53:30 +0000

    variable under stand

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 1234567
+let acoountEmail ="bhandari02@gmial.com"
+var accountPassword ="2347"
+accountCity = "Delhi"
+let accountState ;
+// accountId = 2 // not allowed
+
+
+accountEmail ="bhandarimithlesh02@gmail.com"
+accountPassword = "212121212"
+accountCity ="Bengaluru"
+
+console.log(accountId);
+
+console.table([ accountId,acoountEmail,accountPassword,accountCity ,accountState])
+
+/*
+prefer not use var 
+ because of issue in block scope function scope
+*/
