d319ba7f0e418d7a2910f5d8308057f3f1e0fd65
M bhandari <144377289+M02bhandari@users.noreply.github.com>
2024-06-14 10:18:25 +0000

    database

---
+++ 01_basic/02_database.js
@@ -0,0 +1,22 @@
+"use strict"; //  treat all js codes as a new version
+/*
+prettier code extension used for line bt line code
+*/
+
+let name = "Bhandari"
+let age = 18
+let isLoggedIn = false
+
+
+// types
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+// null => standalone value
+// undefined =>
+    //symbol => use for unique
+
+//object
+
+console.log(typeof null);
