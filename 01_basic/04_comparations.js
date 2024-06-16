daa8dcdedb25fafc812aa9dd8ae73442fd9f3905
M bhandari <144377289+M02bhandari@users.noreply.github.com>
2024-06-16 09:04:20 +0000

    diiff conversion of diff data

---
+++ 01_basic/04_comparations.js
@@ -0,0 +1,15 @@
+//  dont compare different data types
+console.log("2" > 1 );
+console.log("02" > 1);
+
+console.log(null >0);
+console.log(null == 0);
+console.log(null >=0);
+
+console.log(undefined >0);
+console.log(undefined == 0);
+console.log(undefined >=0);
+//  these type of conversion code are confuised 
+// == are differnet syntex than <>greatertan or less than
+
+
