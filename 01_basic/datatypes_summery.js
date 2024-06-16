b05857b2d5169302198b59063209907642d531b9
M bhandari <144377289+M02bhandari@users.noreply.github.com>
2024-06-16 09:35:43 +0000

    datatype summery

---
+++ 01_basic/datatypes_summery.js
@@ -0,0 +1,37 @@
+//  Primitivee data type
+
+//  7 types : string/ numbeer/ boolean/null/undefined/symbol/BigInt
+// example
+const score = 100
+const scoreValue =100.3
+
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail ;
+
+const id = Symbol('123')
+const anotherId = Symbol ('123')
+
+console.log(id === anotherId);
+
+// const bigNumber =834346783189109n
+
+
+
+// Reference /Non primitve 
+
+// Array /object / function /
+
+const heros = ["shaktiman" , "naagraj" , "superman"] 
+let myobj = {
+    name : "Bhandari",
+    age : 22
+
+}
+
+const myfunction = function  (){
+    console.log("hello world");
+}
+
+console.log(typeof anotherId );
+
