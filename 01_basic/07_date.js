9a42c09f3065aa735460e8b6d82f07c77b5f3753
M bhandari <144377289+M02bhandari@users.noreply.github.com>
2024-06-17 08:47:06 +0000

    date

---
+++ 01_basic/07_date.js
@@ -0,0 +1,10 @@
+// DATES
+
+let myDate = new Date ()
+// console.log(myDate.toString ());
+// console.log(myDate.toDateString ());
+// console.log(myDate.toLocaleString ());
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date (2024 , 5,17)
+console.log(myCreatedDate.toDateString());
