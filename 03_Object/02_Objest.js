d6762d4f7c2b907c0bbd41f99c36e22f7d228f7c
M bhandari <144377289+M02bhandari@users.noreply.github.com>
2024-06-20 13:49:16 +0000

    objects

---
+++ 03_Object/02_Objest.js
@@ -0,0 +1,60 @@
+// const tinderUser = new Object () singletune object
+const tinderUser ={}
+tinderUser.id = "12345"
+tinderUser.name ="Golu"
+tinderUser.isLoggedIn = false
+
+
+// console.log(tinderUser);
+
+const regularUser = {
+
+email : "some@gmail.com",
+fullname :{
+    userfullname:{
+        firstname : "Bhandari",
+        lastname : "Mithlesh"
+    }
+
+}
+}
+
+// console.log(regularUser.fullname.userfullname.firstname); 
+
+const obj1 ={ 1: "a", 2: "b"}
+const obj2 ={ 3: "a", 4: "b"}
+const obj4 ={ 5: "a", 6: "b"}
+
+// const obj3 ={ obj1, obj2 }
+//  const obj3 = Object.assign({},obj1,obj2, obj4) 
+ /* { } its use for showing empty object */
+
+ const obj3 = {...obj1, ...obj2, ...obj4}
+//  console.log(obj3);
+
+const user =[
+    {
+        id : 1,
+        email : "bhandari@gmmail.com"
+    },
+    {
+        id : 1,
+        email : "bhandari@gmmail.com"
+    },
+    {
+        id : 1,
+        email : "bhandari@gmmail.com"
+    },
+]
+
+user[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasownproperty('isLoggedIn'));
+
+
+
+
