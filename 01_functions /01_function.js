2f2d3c7c5b29ea977f0853b67b80da5fc1a23ad4
M bhandari <144377289+M02bhandari@users.noreply.github.com>
2024-06-20 15:50:43 +0000

    functions

---
+++ 01_functions /01_function.js
@@ -0,0 +1,18 @@
+/*
+function addTwoNumber(number1,number2) {
+    console.log(number1 + number2);
+}
+
+addTwoNumber (3, 4)
+*/ 
+
+function addTwoNumbers(number1,number2) {
+    let  result = number1 + number2
+    return result 
+    console.log();
+
+}
+
+const result = addTwoNumber(3,5)
+
+console.log("Result:" , result);
