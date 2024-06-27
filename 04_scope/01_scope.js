4236198df4f3ccd5a8b9eebc3295ae516b314fc0
M bhandari <144377289+M02bhandari@users.noreply.github.com>
2024-06-27 12:55:01 +0000

    scope

---
+++ 04_scope/01_scope.js
@@ -0,0 +1,19 @@
+// var c = 300 
+/*
+let a = 300 
+if (true) {
+let a = 30
+const b = 40
+    console.log("INNER: ", a);
+ 
+}
+
+console.log(a);
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+*/
+
