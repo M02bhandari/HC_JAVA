0aaeee319d4a758d3f4316e47b40c8f25afc941d
M bhandari <144377289+M02bhandari@users.noreply.github.com>
2024-06-27 12:14:41 +0000

    if conditon

--- 01_functions /01_function.js
+++ 01_functions /01_function.js
@@ -5,7 +5,7 @@ function addTwoNumber(number1,number2) {
 
 addTwoNumber (3, 4)
 */ 
-
+/* 
 function addTwoNumbers(number1,number2) {
     let  result = number1 + number2
     return result 
@@ -15,4 +15,56 @@ function addTwoNumbers(number1,number2) {
 
 const result = addTwoNumber(3,5)
 
-console.log("Result:" , result);
+// console.log("Result:" , result);
+*/
+
+/*if condition */
+
+/* 
+function loginuserMessage(username) {
+
+    if (username === undefined) {
+        console.log("please logged in");
+        return
+    }
+
+    return `${username} just logged in`
+}
+
+// console.log(loginuserMessage("bhandari"));
+
+console.log(loginuserMessage());
+
+*/
+
+function calculateCartPrice( val1, val2,...num1){ // ... rest operator
+    return num1
+}
+
+// console.log(calculateCartPrice(200,400,500,600,700));
+
+const user = {
+    username: "bhnadari",
+    prices: 199
+}
+function handleobject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+//  handleobject(user)
+handleobject({
+    username: "bhandrai", //  object pass 
+    price: 499
+})
+
+const myNewArray = [200, 300, 400, 600  ]
+function returnSecondValue(getArray) {
+    return getArray[1]
+    
+}
+// console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue[200 , 400, 500, 1000]);
+
+
+
+
